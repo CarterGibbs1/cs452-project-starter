a212439f02fffcdd832004d08d3ac021239977c4
CarterGibbs1 <97470347+CarterGibbs1@users.noreply.github.com>
2024-09-23 20:22:10 +0000

    Added version method

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,27 @@
-/**Update this file with the starter code**/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "lab.h"
+
+void print_version() {
+    printf("Version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+}
+
+int main(int argc, char *argv[]) {
+    int opt;
+    
+    // Use getopt to parse command-line arguments
+    while ((opt = getopt(argc, argv, "v")) != -1) {
+        switch (opt) {
+            case 'v':
+                // Print the version and exit
+                print_version();
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
+                return 1;
+                break;
+        }
+    }
+    return 0;
+}
